6850f1c0b3fc41397a67490a88c7f5d69c7e0452
NikolG <greplova.nikol@gmail.com>
2026-06-09 11:49:06 +0200

    fix in hero description

--- src/data/copy_intro.js
+++ src/data/copy_intro.js
@@ -1,4 +1,4 @@
 export const copy_intro = {
     description:
-        "For years as a designer, I've been thinking in patterns, interactions and purpose. No\u00A0matter which point of view I\u00A0take - data, design or code - I've\u00A0always enjoyed asking why and finding solutions to complex problems. I\u00A0care about how things look and feel, how they function, but also what drives our decisions beneath the surface.",
+        "For years as a designer, I've been thinking in patterns, interactions and purpose. No\u00A0matter\u00A0which point of view I\u00A0take - data, design or code - I've\u00A0always enjoyed asking why and finding solutions to complex problems. I\u00A0care\u00A0about how things look and feel, how they function, but also what drives our decisions beneath the surface.",
 };
