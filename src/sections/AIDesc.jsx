763c40d0ee7a45cba2e198024fd8cdefa3a52d8d
NikolG <greplova.nikol@gmail.com>
2026-06-09 15:48:47 +0200

    copy update

--- src/sections/AIDesc.jsx
+++ src/sections/AIDesc.jsx
@@ -3,11 +3,11 @@ export const AIDesc = () => {
         <div>
             <h2 className="pb-5">AI Usage</h2>
             <p className="leading-normal whitespace-">
-                At the moment, I mostly reach out for Claude when I&nbsp;want
+                At the moment, I mostly reach out for Claude when I want
                 assistance, explanation or a space for experimentation. However,
-                since I&nbsp; am still at the start of my journey, I&nbsp;am
-                quite careful not to overuse these tools while learning and
-                building a solid foundation.
+                since I am still at the start of my journey, I am quite careful
+                not to overuse these tools while learning and building a solid
+                foundation.
                 <br />
                 <br />
                 While I do believe that AI tools offer a huge boost in
--- src/sections/SkillsDesc.jsx
+++ src/sections/SkillsDesc.jsx
@@ -3,11 +3,10 @@ export const SkillsDesc = () => {
         <div>
             <h2 className="pb-5">Skills & Tools</h2>
             <p className="leading-normal">
-                Here are the tools I&nbsp; am&nbsp; actively practicing and
-                using while building projects. Generally, I&nbsp; exist in the
-                Python & Javascript ecosystems -&nbsp; Python being my
-                'thinking' language and Javascript helping me craft good quality
-                public-facing layer.
+                Here are the tools I am actively practicing and using while
+                building projects. Generally, I exist in the Python & Javascript
+                ecosystems - Python being my 'thinking' language and Javascript
+                helping me craft a good quality public-facing layer.
             </p>
         </div>
     );
